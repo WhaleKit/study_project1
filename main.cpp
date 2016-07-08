7c937bbea4c9a3a207924998243d3160d53dfe54
WhaleKit <kitmouse.nikita@gmail.com>
2016-07-09 03:55:06 +0700

    Добавил состояния вместо переменной bool на_земле. Пока что только стоит, идет, в_воздухе. Пока что в виде enum class. Теперь нельзя менять свою скорость в воздухе (т.е. контролировать полет), и ногами он в воздухе не перебирает. Пытаюсь прикртить анимацию стояния на месте, но что-то не срастается.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 
+#include <cmath>
+
 #include <map>
 
 using namespace std;
@@ -40,30 +42,32 @@ public:
 
     void readApplyUserInput(sf::Time lastFrameTime_arg)
     {
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+        if ( sf::Keyboard::isKeyPressed(sf::Keyboard::A)!=Keyboard::isKeyPressed(Keyboard::D) )
         {
-                currentAnimFrame_m += animSpeed_m*lastFrameTime_arg.asMicroseconds();
-        }
-
-        if ( !(sf::Keyboard::isKeyPressed(sf::Keyboard::A)&&Keyboard::isKeyPressed(Keyboard::D) ) )
-        {
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+            if (state_m != State_m::inAir)
             {
-                currentAnimFrame_m+=animSpeed_m*lastFrameTime_arg.asMicroseconds();
-                speed_m.x = -walkingSpeed_m;
-                lookingLeft_m = true;
-            }
-            if (sf::Keyboard::isKeyPressed(Keyboard::D))
-            {
-                currentAnimFrame_m+=animSpeed_m*lastFrameTime_arg.asMicroseconds();
-                speed_m.x = walkingSpeed_m;
-                lookingLeft_m = false;
+                if ( sf::Keyboard::isKeyPressed(sf::Keyboard::A) )
+                {
+                    speed_m.x = -walkingSpeed_m;
+                    lookingLeft_m = true;
+                }
+                else
+                {
+                    speed_m.x = walkingSpeed_m;
+                    lookingLeft_m = false;
+                }
+                state_m = State_m::walking;
             }
         }
-        if (    ((sf::Keyboard::isKeyPressed(Keyboard::W))||(sf::Keyboard::isKeyPressed(Keyboard::Space)))
-                && onGround_m )
+        else if (state_m != State_m::inAir)
         {
-            onGround_m=false;
+            state_m = State_m::standing;
+        }
+        if  (   ((sf::Keyboard::isKeyPressed(Keyboard::W))||(sf::Keyboard::isKeyPressed(Keyboard::Space)))
+                && state_m!= State_m::inAir
+            )
+        {
+            state_m = State_m::inAir;
             speed_m.y = -jumpingSpeed_m;
         }
     }
@@ -72,8 +76,12 @@ public:
     {
         readApplyUserInput(frameTime_arg);
 
+        currentAnimFrame_m+=animSpeed_m*frameTime_arg.asMicroseconds();
+
         collizion_m->left += speed_m.x * frameTime_arg.asMicroseconds();
-        speed_m.x = 0;
+
+        if (state_m == State_m::standing)
+            speed_m.x = 0;
 
         collizion_m->top += speed_m.y * frameTime_arg.asMicroseconds();
 
@@ -86,7 +94,11 @@ public:
         {
             collizion_m->top = floorLevel-collizion_m->height;
             speed_m.y = max(0.0f, speed_m.y);
-            onGround_m=true;
+            if (speed_m.x == 0)
+                state_m = State_m::standing;
+            else
+                state_m = State_m::walking;
+            //onGround_m=true;
         }
 
     }
@@ -94,18 +106,38 @@ public:
     sf::Drawable* getDrawableComponent() override
     {
         //приводим спрайт в соотв. с состоянием персонажа и отдаем
-        if (currentAnimFrame_m>6)
-            currentAnimFrame_m-=6;
 
 
         renderComponent_m->setPosition( Vector2f(collizion_m->left, collizion_m->top)
                                         +spriteCoordRelativeToCollision_m
                                       );
+        if (state_m == State_m::walking)
+        {
+            if (currentAnimFrame_m>6)
+                currentAnimFrame_m = fmod (currentAnimFrame_m, 6);
+            if (lookingLeft_m)
+                renderComponent_m->setTextureRect(IntRect(40+40*int(currentAnimFrame_m), 244, -40, 50));
+            else
+                renderComponent_m->setTextureRect(IntRect(40*int(currentAnimFrame_m), 244, 40, 50));
+        }
+        else if (state_m == State_m::standing)
+        {
+            if (currentAnimFrame_m>4)
+                currentAnimFrame_m = fmod (currentAnimFrame_m, 4.0f);
 
-        if (lookingLeft_m)
-            renderComponent_m->setTextureRect(IntRect(40+40*int(currentAnimFrame_m), 244, -40, 50));
-        else
-            renderComponent_m->setTextureRect(IntRect(40*int(currentAnimFrame_m), 244, 40, 50));
+            if (currentAnimFrame_m>3.0)
+            {
+                if (lookingLeft_m)
+                    renderComponent_m->setTextureRect(IntRect(44+44*int(5.0f-currentAnimFrame_m), 189, -44, 50));
+                else
+                    renderComponent_m->setTextureRect(IntRect(44*int(5.0f-currentAnimFrame_m), 189, 44, 50));
+            }
+
+            if (lookingLeft_m)
+                renderComponent_m->setTextureRect(IntRect(44+44*int(currentAnimFrame_m), 189, -44, 50));
+            else
+                renderComponent_m->setTextureRect(IntRect(44*int(currentAnimFrame_m), 189, 44, 50));
+        }
 
 
         return renderComponent_m;
@@ -114,12 +146,17 @@ public:
     Vector2f spriteCoordRelativeToCollision_m = Vector2f(0,0);
     //смещение левого верхнего угла спрайта от верхнего левого угла коллизии
     float currentAnimFrame_m=0;
+
     bool onGround_m         = false;
+
+    enum class State_m {walking, standing, inAir};
+    State_m state_m         = State_m::standing;
+
     bool lookingLeft_m      = false; //в противном случае смотрит вправо
 
-    Vector2f speed_m         = Vector2f(0,0);
+    Vector2f speed_m        = Vector2f(0,0);
 
-    float jumpingSpeed_m        = 0.002; //вертикальная скорость, которая ему придается при прыжке
+    float jumpingSpeed_m        = 0.0018; //вертикальная скорость, которая ему придается при прыжке
     float walkingSpeed_m        = 0.0006;  //скорость, с которой он ходит
     float animSpeed_m           = 0.00001; // смен кадров в микросекунду
     sf::FloatRect* collizion_m  = nullptr;
@@ -140,7 +177,7 @@ int main()
     sf::Sprite sprt;
     sprt.setTexture(myTxtr);
     sprt.setTextureRect( IntRect(0, 244, 40, 50) );
-    sprt.setScale(2,2);
+    sprt.setScale(3,3);
     sprt.setPosition(30, 40);
 
     FloatRect FangCollizion = sprt.getGlobalBounds();
